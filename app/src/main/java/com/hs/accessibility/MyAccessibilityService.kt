dd1953ac6d9f675b43336d7757ecca98c7045be4
aptx113 <aptx113@gmail.com>
2022-01-05 16:09:09 +0800

    initialize project

---
+++ app/src/main/java/com/hs/accessibility/MyAccessibilityService.kt
@@ -0,0 +1,60 @@
+package com.hs.accessibility
+
+import android.accessibilityservice.AccessibilityService
+import android.os.Bundle
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+
+class MyAccessibilityService : AccessibilityService() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("ORZ", "create")
+    }
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        Log.d("GG service on", "service on")
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        val rootInActiveWindow = rootInActiveWindow
+
+        if (rootInActiveWindow != null) {
+            val searchBarIdle =
+                rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.google.android.apps.nexuslauncher:id/g_icon")
+            if (searchBarIdle.size > 0) {
+                val searchBar = searchBarIdle[0]
+                searchBar.parent.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+            }
+            val searchBars =
+                rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.google.android.googlequicksearchbox:id/googleapp_search_box")
+            if (searchBars.size > 0) {
+                val searchBar = searchBars[0]
+                if (searchBar.text == null || !searchBar.text.toString()
+                        .equals("Messages", ignoreCase = true)
+                ) {
+                    val args = Bundle()
+                    args.putString(
+                        AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE,
+                        "messages"
+                    )
+                    searchBar.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, args)
+                } else {
+                    val searchSuggestions =
+                        rootInActiveWindow.findAccessibilityNodeInfosByViewId("com.google.android.googlequicksearchbox:id/googleapp_app_name")
+                    searchSuggestions.forEach {
+                        if (it.text.toString() == "Messages") {
+                            val clickableParent = it.parent
+                            clickableParent.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                        }
+                    }
+                }
+                searchBar.recycle()
+            }
+        }
+    }
+    override fun onInterrupt() {
+    }
+}
